deea69aec9e58bdbaf1e6ea9a30b22848e3e36a2
Cargalher <cargalher@gmail.com>
2021-06-23 15:47:48 +0200

    foreach  loop

--- index.php
+++ index.php
@@ -53,3 +53,19 @@ $faqs = [
 ];
 var_dump($faqs);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php foreach ($faqs as  $faq) 
+    echo $faq;
+?>
+  
+
+</body>
+</html>
